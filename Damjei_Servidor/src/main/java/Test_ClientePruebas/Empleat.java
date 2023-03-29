07aad0b9d6aaa400434cc815f08dfc4b00dbfec7
ivansh75 <126617261+ivansh75@users.noreply.github.com>
2023-03-29 21:09:52 +0200

    Actualizado con Login

---
+++ Damjei_Servidor/src/main/java/Test_ClientePruebas/Empleat.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Test_ClientePruebas;
+
+/**
+ *
+ * @author Ivimar
+ */
+public class Empleat {
+    
+    private String nom;
+    private String contrasenya;
+    private int id_empresa;
+
+     // getters y setters
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getContrasenya() {
+        return contrasenya;
+    }
+
+    public void setContrasenya(String contrasenya) {
+        this.contrasenya = contrasenya;
+    }
+
+    public int getId_empresa() {
+        return id_empresa;
+    }
+
+    public void setId_empresa(int id_empresa) {
+        this.id_empresa = id_empresa;
+    }
+    
+    
+   
+}
