574df5c3a3d996fe80b008b2c013e4b1562de178
ivansh75 <126617261+ivansh75@users.noreply.github.com>
2023-04-14 21:23:20 +0200

    gestion combustible ok testeado con nombre

---
+++ Damjei_Servidor/src/test/java/TestServidor_Cliente/TestCombustible.java
@@ -0,0 +1,341 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package TestServidor_Cliente;
+
+import Test_ClientePruebas.CombustibleObjeto;
+import Test_ClientePruebas.DatosCombustible;
+import Test_ClientePruebas.DatosEmpleado;
+import Test_ClientePruebas.Empleat;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import damjei_Servidor_Run.Server;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+/**
+ *
+ * @author Ivimar
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestCombustible {
+
+    private static String testToken;
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    //instacia del servidor para gestionar el test
+    Server instance = new Server();
+    boolean testCorrecte = true;
+
+    /**
+     * Primer Test Arrancamos servidor primero
+     *
+     * @throws IOException
+     */
+    @Test
+    public void TestA() throws IOException {
+        System.out.println("---------TEST A-------------");
+        System.out.println("-----INICIAR SERVIDOR-------");
+
+        instance.initialize("localhost", 8180);
+        instance.run();
+
+        boolean repuesta = instance.isRunning();
+        Assert.assertEquals(repuesta, instance.isRunning());
+
+    }
+
+    /**
+     *
+     * Segundo Test Iniciamos primero usuario como administrador
+     *
+     * @throws IOException
+     */
+    @Test
+    public void TestB() throws IOException {
+        boolean correcte = false;
+
+        System.out.println("-------------------TEST B-----------------------");
+        System.out.println("-----INICIAR SESION CLIENTE ADMINISTRADOR-------");
+
+        // Crear una instancia de la clase y establecer sus valores
+        DatosEmpleado datos = new DatosEmpleado();
+        Empleat empleat = new Empleat();
+        empleat.setDni("admin");
+        empleat.setContrasenya("admin1");
+        empleat.setId_empresa(1);
+        datos.setEmpleat(empleat);
+        datos.setAccio(0);
+        datos.setClase("Empleats.class");
+
+        // Serializar el objeto en formato JSON usando Gson
+        Gson gson = new Gson();
+        String json = gson.toJson(datos);
+        // Enviar los datos al servidor usando una conexión de socket
+        Socket socket = new Socket("localhost", 8180);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        out.println(json);
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        String data = input.readLine();
+
+        JsonObject jsonObject = new Gson().fromJson(data, JsonObject.class);
+        // Obtener el Token para las pruebas
+        JsonElement testJson = jsonObject.get("token");
+        JsonElement correctaJson = jsonObject.get("correcte");
+        if (testJson != null && !testJson.isJsonNull()) {
+            testToken = testJson.getAsString();
+            correcte = correctaJson.getAsBoolean();
+
+        }
+        socket.close();
+        Assert.assertEquals(testCorrecte, correcte);
+
+    }
+
+    /**
+     * Tercer Test para insertar un combustible nuevo
+     *
+     * @throws IOException
+     */
+    @Test
+    public void TestC() throws IOException {
+        boolean correcte = false;
+
+        System.out.println("--------------TEST C------------------");
+        System.out.println("-----INSERTAR NUEVO COMBUSTIBLE-------");
+
+        // Crear una instancia de la clase y establecer sus valores
+        DatosCombustible datos = new DatosCombustible();
+
+        CombustibleObjeto comObj = new CombustibleObjeto();
+        comObj.setIdcombustible(8);
+        comObj.setNombre("DieselPlus");
+        comObj.setPrecio(Float.valueOf("1.90"));
+        datos.setCombustible(comObj);
+        datos.setAccio(2);
+        datos.setClase("Combustible.class");
+        datos.setToken(testToken);
+
+        // Serializar el objeto en formato JSON usando Gson
+        Gson gson = new Gson();
+        String json = gson.toJson(datos);
+
+        // Enviar los datos al servidor usando una conexión de socket
+        Socket socket = new Socket("localhost", 8180);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        out.println(json);
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        String data = input.readLine();
+
+        JsonObject jsonObject = new Gson().fromJson(data, JsonObject.class);
+        JsonElement correctaJson = jsonObject.get("correcte");
+        if (correctaJson != null && !correctaJson.isJsonNull()) {
+            correcte = correctaJson.getAsBoolean();
+
+        }
+
+        socket.close();
+
+        Assert.assertEquals(testCorrecte, correcte);
+
+    }
+
+    /**
+     * Cuarto Test para Actualizar un combustible
+     *
+     * @throws IOException
+     */
+    @Test
+    public void TestD() throws IOException {
+        boolean correcte = false;
+
+        System.out.println("-----------TEST D-----------------");
+        System.out.println("-----ACTUALIZAR COMBUSTIBLE-------");
+        // Crear una instancia de la clase y establecer sus valores
+        DatosCombustible datos = new DatosCombustible();
+
+        CombustibleObjeto comObj = new CombustibleObjeto();
+        comObj.setNombre("Diesel");
+        datos.setCombustible(comObj);
+        datos.setAccio(3);
+        datos.setClase("Combustible.class");
+        datos.setToken(testToken);
+
+        // Serializar el objeto en formato JSON usando Gson
+        Gson gson = new Gson();
+        String json = gson.toJson(datos);
+
+        // Enviar los datos al servidor usando una conexión de socket
+        Socket socket = new Socket("localhost", 8180);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        out.println(json);
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        String data = input.readLine();
+
+        JsonObject jsonObject = new Gson().fromJson(data, JsonObject.class);
+        JsonElement correctaJson = jsonObject.get("correcte");
+        if (correctaJson != null && !correctaJson.isJsonNull()) {
+            correcte = correctaJson.getAsBoolean();
+
+        }
+
+        socket.close();
+        Assert.assertEquals(testCorrecte, correcte);
+
+    }
+
+    /**
+     *
+     * Quinto Test para Mostrar Lista de Combustibles
+     * Podremos ir viendo como se añaden, eliminan y modifican los combustibles
+     *
+     * @throws IOException
+     */
+    @Test
+    public void TestE() throws IOException {
+        boolean correcte = false;
+
+        System.out.println("--------------TEST E------------------");
+        System.out.println("-----MOSTRAR LISTA DE COMBUSTIBLES-------");
+
+        // Crear una instancia de la clase y establecer sus valores
+        DatosCombustible datos = new DatosCombustible();
+
+        CombustibleObjeto comObj = new CombustibleObjeto();
+        comObj.setNombre("Diesel");
+        datos.setCombustible(comObj);
+        datos.setAccio(5);
+        datos.setClase("Combustible.class");
+        datos.setToken(testToken);
+
+        // Serializar el objeto en formato JSON usando Gson
+        Gson gson = new Gson();
+        String json = gson.toJson(datos);
+
+        // Enviar los datos al servidor usando una conexión de socket
+        Socket socket = new Socket("localhost", 8180);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        out.println(json);
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        String data = input.readLine();
+
+        String jsonR = data.toString();
+
+        socket.close();
+
+        Assert.assertEquals(jsonR, data);
+
+    }
+
+    /**
+     * Sexto Test para Eliminar un combustible
+     *
+     * @throws IOException
+     */
+    @Test
+    public void TestF() throws IOException {
+        boolean correcte = false;
+
+        System.out.println("-----------TEST F----------------");
+        System.out.println("-----ELIMINAR COMBUSTIBLES-------");
+
+        // Crear una instancia de la clase y establecer sus valores
+        DatosCombustible datos = new DatosCombustible();
+
+        CombustibleObjeto comObj = new CombustibleObjeto();
+        comObj.setIdcombustible(8);
+        comObj.setNombre("DieselPlus");
+        datos.setCombustible(comObj);
+        datos.setAccio(4);
+        datos.setClase("Combustible.class");
+        datos.setToken(testToken);
+
+        // Serializar el objeto en formato JSON usando Gson
+        Gson gson = new Gson();
+        String json = gson.toJson(datos);
+
+        // Enviar los datos al servidor usando una conexión de socket
+        Socket socket = new Socket("localhost", 8180);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        out.println(json);
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        String data = input.readLine();
+
+        JsonObject jsonObject = new Gson().fromJson(data, JsonObject.class);
+        JsonElement correctaJson = jsonObject.get("correcte");
+        if (correctaJson != null && !correctaJson.isJsonNull()) {
+            correcte = correctaJson.getAsBoolean();
+
+        }
+
+        socket.close();
+
+        Assert.assertEquals(testCorrecte, correcte);
+        TestE();
+    }
+
+    /**
+     * Septimo Test para Modificar un Combustible
+     *
+     * @throws IOException
+     */
+    @Test
+    public void TestG() throws IOException {
+        boolean correcte = false;
+
+        System.out.println("---------------TEST G--------------------");
+        System.out.println("--------MODIFICAR COMBUSTIBLE------------");
+
+        // Crear una instancia de la clase y establecer sus valores
+        DatosCombustible datos = new DatosCombustible();
+
+        CombustibleObjeto comObj = new CombustibleObjeto();
+        comObj.setIdcombustible(8);
+        comObj.setNombre("Diesel");
+        comObj.setPrecio(Float.valueOf("1.10"));
+        datos.setCombustible(comObj);
+        datos.setAccio(6);
+        datos.setClase("Combustible.class");
+        datos.setToken(testToken);
+        // Serializar el objeto en formato JSON usando Gson
+        Gson gson = new Gson();
+        String json = gson.toJson(datos);
+
+        // Enviar los datos al servidor usando una conexión de socket
+        Socket socket = new Socket("localhost", 8180);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+        out.println(json);
+
+        BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+        String data = input.readLine();
+        JsonObject jsonObject = new Gson().fromJson(data, JsonObject.class);
+        JsonElement correctaJson = jsonObject.get("correcte");
+        if (correctaJson != null && !correctaJson.isJsonNull()) {
+            correcte = correctaJson.getAsBoolean();
+
+        }
+        socket.close();
+        Assert.assertEquals(testCorrecte, correcte);
+        TestE();
+    }
+
+}
