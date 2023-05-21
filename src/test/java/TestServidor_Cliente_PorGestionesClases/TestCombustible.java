9e19811ccc0e45ded6968099379d4d130444bc0d
ivansh75 <126617261+ivansh75@users.noreply.github.com>
2023-05-21 12:46:23 +0200

    21/05/23 Test acciones OK
    Los test de acciones funcionan y los test por clase u objeto faltan de modificar, Revisiones, Vehículos, Mantenimientos

---
+++ src/test/java/TestServidor_Cliente_PorGestionesClases/TestCombustible.java
@@ -0,0 +1,341 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package TestServidor_Cliente_PorGestionesClases;
+
+import TestServidor_Cliente_PorAcciones.TestEliminar;
+import TestServidor_Cliente_PorAcciones.TestInsertar;
+import TestServidor_Cliente_PorAcciones.TestListar;
+import TestServidor_Cliente_PorAcciones.TestListarId;
+import TestServidor_Cliente_PorAcciones.TestModificar;
+import TestServidor_Cliente_Resultado_Acciones.ResultadoInsertDeleteUpdateTest;
+import TestServidor_Cliente_Resultado_Acciones.ResultadoListarListarIdTest;
+import TestSocketSSL_Client.SocketSSL_Cliente;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import damjei_ConexionCliente.Encriptador;
+import static damjei_GestionClases.GestionCombustible.COMBUSTIBLE;
+import static damjei_GestionClases.GestionEmpleados.ELIMINAR;
+import static damjei_GestionClases.GestionEmpleados.INSERTAR;
+import static damjei_GestionClases.GestionEmpleados.MODIFICAR;
+import damjei_Servidor_Run.Server_SSLSocket;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.UnrecoverableKeyException;
+import java.security.cert.CertificateException;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+
+/**
+ *
+ * @author Ivimar
+ */
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class TestCombustible {
+
+    private static String testToken;
+    SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+    private static String HOST = "localhost";
+    private static int PUERTO = 8180;
+    //instacia del servidor para gestionar el test
+    Server_SSLSocket instance = new Server_SSLSocket();
+    boolean testCorrecte = true;
+    boolean correcte = false;
+    //instanciamos los resultados posibles del test
+    ResultadoInsertDeleteUpdateTest testResult = new ResultadoInsertDeleteUpdateTest();
+    ResultadoListarListarIdTest testResultList = new ResultadoListarListarIdTest();
+
+    /**
+     * Primer Test Arrancamos servidor primero
+     *
+     * @throws IOException
+     */
+    @Test
+    public void TestA() throws IOException {
+
+        System.out.println("---------TEST A-------------");
+        System.out.println("-----INICIAR SERVIDOR-------");
+
+        instance.initialize(HOST, PUERTO);
+        instance.run();
+
+        boolean repuesta = instance.isRunning();
+        Assert.assertEquals(repuesta, instance.isRunning());
+
+    }
+
+    /**
+     *
+     * Segundo Test Iniciamos primero usuario como administrador
+     *
+     * @throws IOException
+     */
+    @Test
+    public void TestB() throws IOException {
+
+        try {
+
+            System.out.println("-------------------TEST B-----------------------");
+            System.out.println("-----INICIAR SESION CLIENTE ADMINISTRADOR-------");
+
+            Encriptador hash = new Encriptador();
+            String contraseña = hash.encriptarConSha256("admin1");
+            // Serializar el objeto en formato de Gson
+            String datos = "{\"empleat\":{\"datos\":[],\"dni\":\"admin\",\"contraseña\":" + contraseña + "},\"clase\":\"Empleats.class\",\"accio\":0} ";
+
+            // Enviar los datos al servidor usando una conexión de socket
+            SocketSSL_Cliente empleadoSSL = new SocketSSL_Cliente();
+            Socket socket = empleadoSSL.connect(HOST, PUERTO);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(datos);
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            String data = input.readLine();
+
+            JsonObject jsonObject = new Gson().fromJson(data, JsonObject.class);
+            // Obtener el Token para las pruebas
+            JsonElement testJson = jsonObject.get("token");
+            JsonElement correctaJson = jsonObject.get("correcte");
+            if (testJson != null && !testJson.isJsonNull()) {
+                testToken = testJson.getAsString();
+                correcte = correctaJson.getAsBoolean();
+
+            }
+            socket.close();
+            Assert.assertEquals(testCorrecte, correcte);
+        } catch (KeyStoreException | FileNotFoundException | CertificateException | UnrecoverableKeyException | KeyManagementException | NoSuchAlgorithmException ex) {
+            Logger.getLogger(TestCombustible.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     * Tercero Test para insertar un combustible nuevo
+     *
+     * @throws IOException
+     * @throws java.sql.SQLException
+     */
+    @Test
+    public void TestC() throws IOException, SQLException {
+        String nombre = "DieselPlus";
+        try {
+
+            System.out.println("--------------TEST C------------------");
+            System.out.println("-----INSERTAR NUEVO COMBUSTIBLE-------");
+
+            // Serializar el objeto en formato de Gson
+            //String datos = "{\"combustible\":{\"datosCom\":[],\"idcombustible\":8,\"nombre\":\"DieselPlus\",\"precio\":1.9},\"clase\":\"Combustible.class\",\"token\":" + testToken + ",\"accio\":2}";
+            String datos = "{\"combustible\":{\"datosCom\":[],\"idcombustible\":8,\"nombre\":" + nombre + ",\"precio\":1.9},\"clase\":\"Combustible.class\",\"token\":" + testToken + ",\"accio\":2}";
+
+            // Enviar los datos al servidor usando una conexión de socket
+            SocketSSL_Cliente empleadoSSL = new SocketSSL_Cliente();
+            Socket socket = empleadoSSL.connect(HOST, PUERTO);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(datos);
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String data = input.readLine();
+
+            JsonObject jsonObject = new Gson().fromJson(data, JsonObject.class);
+            JsonElement correctaJson = jsonObject.get("correcte");
+            if (correctaJson != null && !correctaJson.isJsonNull()) {
+                correcte = correctaJson.getAsBoolean();
+
+            }
+            socket.close();
+            boolean result = testResult.ComprobarResultadosCombustible(nombre, INSERTAR);//obtenemos el resultado de la comprobacion si se ha insertado result=true  
+            Assert.assertEquals(result, correcte);
+
+        } catch (KeyStoreException | FileNotFoundException | CertificateException | UnrecoverableKeyException | KeyManagementException | NoSuchAlgorithmException ex) {
+            Logger.getLogger(TestInsertar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     *
+     * Cuarot Test para Mostrar Lista de Combustibles Podremos ir viendo como se
+     * añaden, eliminan y modifican los combustibles
+     *
+     * @throws IOException
+     * @throws java.sql.SQLException
+     */
+    @Test
+    public void TestD() throws IOException, SQLException {
+
+        try {
+
+            System.out.println("--------------TEST D------------------");
+            System.out.println("-----MOSTRAR LISTA DE COMBUSTIBLES-------");
+
+            // Serializar el objeto en formato de Gson
+            String datos = "{\"combustible\":{\"datosCom\":[]},\"clase\":\"Combustible.class\",\"token\":" + testToken + ",\"accio\":5}";
+
+            // Enviar los datos al servidor usando una conexión de socket
+            SocketSSL_Cliente empleadoSSL = new SocketSSL_Cliente();
+            Socket socket = empleadoSSL.connect(HOST, PUERTO);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(datos);
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String data = input.readLine();
+
+            int length = data.length();
+
+            socket.close();
+
+            int result = testResultList.ComprobarListarCombustible(COMBUSTIBLE);//obtenemos la medida del String              
+            Assert.assertEquals(result, length);
+
+        } catch (KeyStoreException | FileNotFoundException | CertificateException | UnrecoverableKeyException | KeyManagementException | NoSuchAlgorithmException ex) {
+            Logger.getLogger(TestListar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     * Quinto Test para Eliminar un combustible
+     *
+     * @throws IOException
+     * @throws java.sql.SQLException
+     */
+    @Test
+    public void TestE() throws IOException, SQLException {
+        String nombre = "DieselPlus";
+        try {
+
+            System.out.println("-----------TEST E----------------");
+            System.out.println("-----ELIMINAR COMBUSTIBLES-------");
+
+            // Serializar el objeto en formato de Gson
+            //String datos = "{\"combustible\":{\"datosCom\":[],\"nombre\":\"DieselPlus\"},\"clase\":\"Combustible.class\",\"token\":" + testToken + ",\"accio\":4}";
+            String datos = "{\"combustible\":{\"datosCom\":[],\"nombre\":" + nombre + "},\"clase\":\"Combustible.class\",\"token\":" + testToken + ",\"accio\":4}";
+
+            // Enviar los datos al servidor usando una conexión de socket
+            SocketSSL_Cliente empleadoSSL = new SocketSSL_Cliente();
+            Socket socket = empleadoSSL.connect(HOST, PUERTO);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(datos);
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String data = input.readLine();
+
+            JsonObject jsonObject = new Gson().fromJson(data, JsonObject.class);
+            JsonElement correctaJson = jsonObject.get("correcte");
+            if (correctaJson != null && !correctaJson.isJsonNull()) {
+                correcte = correctaJson.getAsBoolean();
+
+            }
+
+            socket.close();
+            boolean result = testResult.ComprobarResultadosCombustible(nombre, ELIMINAR);//obtenemos el resultado de la comprobacion si se ha borrado result=true  
+            Assert.assertEquals(result, correcte);
+
+        } catch (KeyStoreException | FileNotFoundException | CertificateException | UnrecoverableKeyException | KeyManagementException | NoSuchAlgorithmException ex) {
+            Logger.getLogger(TestEliminar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     * Sexto Test para Modificar un Combustible
+     *
+     * @throws IOException
+     * @throws java.sql.SQLException
+     */
+    @Test
+    public void TestF() throws IOException, SQLException {
+        String precio = "1.85";
+
+        try {
+
+            System.out.println("---------------TEST F--------------------");
+            System.out.println("--------MODIFICAR COMBUSTIBLE------------");
+
+            // Serializar el objeto en formato de Gson
+            //String datos = "{\"combustible\":{\"datosCom\":[],\"nombre\":\"Diesel\",\"precio\":1.80},\"clase\":\"Combustible.class\",\"token\":" + testToken + ",\"accio\":6}";
+            String datos = "{\"combustible\":{\"datosCom\":[],\"nombre\":\"Diesel\",\"precio\":" + precio + "},\"clase\":\"Combustible.class\",\"token\":" + testToken + ",\"accio\":6}";
+
+            // Enviar los datos al servidor usando una conexión de socket
+            SocketSSL_Cliente empleadoSSL = new SocketSSL_Cliente();
+            Socket socket = empleadoSSL.connect(HOST, PUERTO);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(datos);
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String data = input.readLine();
+            JsonObject jsonObject = new Gson().fromJson(data, JsonObject.class);
+            JsonElement correctaJson = jsonObject.get("correcte");
+            if (correctaJson != null && !correctaJson.isJsonNull()) {
+                correcte = correctaJson.getAsBoolean();
+
+            }
+            socket.close();
+            boolean result = testResult.ComprobarResultadosCombustible(precio, MODIFICAR);//obtenemos el resultado de la comprobacion si se ha insertado result=true  
+            Assert.assertEquals(result, correcte);
+        } catch (KeyStoreException | FileNotFoundException | CertificateException | UnrecoverableKeyException | KeyManagementException | NoSuchAlgorithmException ex) {
+            Logger.getLogger(TestModificar.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+    /**
+     * Septimo Test Mostrar un combustible segun id
+     *
+     * @throws IOException
+     * @throws java.sql.SQLException
+     */
+    @Test
+    public void TestH() throws IOException, SQLException {
+        String idcombustible = "1";
+        try {
+
+            System.out.println("------------------TEST H--------------------");
+            System.out.println("-----MOSTRAR LISTA DE UN COMBUSTIBLES-------");
+
+            // Serializar el objeto en formato de Gson
+            String datos = "{\"combustible\":{\"datosCom\":[],\"idcombustible\":1},\"clase\":\"Combustible.class\",\"token\":" + testToken + ",\"accio\":7}";
+
+            // Enviar los datos al servidor usando una conexión de socket
+            SocketSSL_Cliente empleadoSSL = new SocketSSL_Cliente();
+            Socket socket = empleadoSSL.connect(HOST, PUERTO);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+            out.println(datos);
+
+            BufferedReader input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String data = input.readLine();
+
+            int length = data.length();
+
+            socket.close();
+
+            int result = testResultList.ComprobarListarCombustible(idcombustible);//obtenemos la medida del String              
+            Assert.assertEquals(result, length);
+
+        } catch (KeyStoreException | FileNotFoundException | CertificateException | UnrecoverableKeyException | KeyManagementException | NoSuchAlgorithmException ex) {
+            Logger.getLogger(TestListarId.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+}
---
+++ src/test/java/TestServidor_Cliente_Resultado_Acciones/ResultadoInsertDeleteUpdateTest.java
@@ -0,0 +1,340 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package TestServidor_Cliente_Resultado_Acciones;
+
+import damjei_BD.ConexionBD;
+import static damjei_GestionClases.GestionEmpleados.ELIMINAR;
+import static damjei_GestionClases.GestionEmpleados.INSERTAR;
+import static damjei_GestionClases.GestionEmpleados.MODIFICAR;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Objects;
+
+/**
+ *
+ * @author Ivimar
+ */
+public class ResultadoInsertDeleteUpdateTest {
+
+    PreparedStatement ps = null;
+    ResultSet rs = null;
+
+    ConexionBD cn = new ConexionBD();
+    Connection con = cn.establecerConexion();
+
+    public ResultadoInsertDeleteUpdateTest() {
+    }
+
+    /**
+     * prueba de resultado Tercer Test para Eliminar un empleado prueba de
+     * resultado Tercer Test para Listar un empleado
+     *
+     * @param variable
+     * @param accion
+     * @return
+     * @throws IOException
+     * @throws java.sql.SQLException
+     */
+    public Boolean ComprobarResultadosEmpleado(String variable, int accion) throws IOException, SQLException {
+        boolean prueba = false;
+
+        String sqli = """
+                                      SELECT * FROM public.empleados
+                                       ORDER BY idempleado ASC""";
+
+        ps = con.prepareStatement(sqli);
+        rs = ps.executeQuery();
+
+        while (rs.next()) {
+            switch (accion) {
+                case ELIMINAR:
+                    if (!variable.equalsIgnoreCase(rs.getString("dni"))) {
+                        prueba = true;
+                    }
+                case INSERTAR:
+                    if (variable.equalsIgnoreCase(rs.getString("dni"))) {
+                        prueba = true;
+                    }
+                case MODIFICAR:
+                    if (variable.equalsIgnoreCase(rs.getString("categoria"))) {
+                        prueba = true;
+                    }
+            }
+        }
+        rs.close();
+        ps.close(); // Cerrar el PreparedStatement
+        cn.cerrarConexion();
+
+        return prueba;
+
+    }
+
+    /**
+     * Prueba de resultado Cuarto Test para Eliminar un vehiculo Prueba de
+     * resultado Cuarto Test para Listar un vehiculo
+     *
+     * @param variable
+     * @param accion
+     * @return
+     * @throws IOException
+     * @throws java.sql.SQLException
+     */
+    public Boolean ComprobarResultadosVehiculo(String variable, int accion) throws IOException, SQLException {
+        boolean prueba = false;
+
+        String sqli = """
+                                      SELECT * FROM public.vehiculos
+                                       ORDER BY idvehiculo ASC""";
+
+        ps = con.prepareStatement(sqli);
+        rs = ps.executeQuery();
+
+        while (rs.next()) {
+            switch (accion) {
+                case ELIMINAR:
+                    if (!variable.equalsIgnoreCase(rs.getString("matricula"))) {
+                        prueba = true;
+                    }
+                case INSERTAR:
+                    if (variable.equalsIgnoreCase(rs.getString("matricula"))) {
+                        prueba = true;
+                    }
+
+                case MODIFICAR:
+                    if (variable.equalsIgnoreCase(rs.getString("fecha_baja"))) {
+                        prueba = true;
+                    }
+
+            }
+        }
+
+        rs.close();
+        ps.close(); // Cerrar el PreparedStatement
+        cn.cerrarConexion();
+
+        return prueba;
+
+    }
+
+    /**
+     * Prueba de resultado Quinto Test para Eliminar un Repostaje Prueba de
+     * resultado Quinto Test para Listar un Repostaje
+     *
+     * @param variable
+     * @param accion
+     * @return
+     * @throws IOException
+     * @throws java.sql.SQLException
+     */
+    public Boolean ComprobarResultadosRepostar(String variable, int accion) throws IOException, SQLException {
+        boolean prueba = false;
+
+        String sqli = """
+                                      SELECT * FROM public.repostar
+                                       ORDER BY idrepostar ASC""";
+
+        ps = con.prepareStatement(sqli);
+        rs = ps.executeQuery();
+
+        while (rs.next()) {
+            switch (accion) {
+                case ELIMINAR:
+                    if (!variable.equalsIgnoreCase(String.valueOf(rs.getString("idrepostar")))) {
+                        prueba = true;
+                    }
+                case INSERTAR:
+                    if (variable.equalsIgnoreCase(String.valueOf(rs.getString("idrepostar")))) {
+                        prueba = true;
+                    }
+                case MODIFICAR:
+                    if (variable.equalsIgnoreCase(String.valueOf(rs.getString("conductorid")))) {
+                        prueba = true;
+                    }
+            }
+        }
+
+        rs.close();
+        ps.close(); // Cerrar el PreparedStatement
+        cn.cerrarConexion();
+
+        return prueba;
+
+    }
+
+    /**
+     * Prueba de resultado Sexto Test para Eliminar una Revision Prueba de
+     * resultado Sexto Test para Listar una Revision
+     *
+     * @param variable
+     * @param accion
+     * @return
+     * @throws IOException
+     * @throws java.sql.SQLException
+     */
+    public Boolean ComprobarResultadosRevison(String variable, int accion) throws IOException, SQLException {
+        boolean prueba = false;
+
+        String sqli = """
+                                      SELECT * FROM public.revisiones
+                                       ORDER BY idrevision ASC""";
+
+        ps = con.prepareStatement(sqli);
+        rs = ps.executeQuery();
+
+        while (rs.next()) {
+            switch (accion) {
+                case ELIMINAR:
+                    if (!variable.equalsIgnoreCase(String.valueOf(rs.getString("idrevision")))) {
+                        prueba = true;
+                    }
+                case INSERTAR:
+                    if (variable.equalsIgnoreCase(String.valueOf(rs.getString("idrevision")))) {
+                        prueba = true;
+                    }
+                case MODIFICAR:
+                    if (variable.equalsIgnoreCase(String.valueOf(rs.getString("mantenimientoid")))) {
+                        prueba = true;
+                    }
+            }
+        }
+
+        rs.close();
+        ps.close(); // Cerrar el PreparedStatement
+        cn.cerrarConexion();
+
+        return prueba;
+
+    }
+
+    /**
+     * Prueba de resultado Septimo Test para Eliminar un Mantenimiento Prueba de
+     * resultado Septimo Test para Listar un Mantenimiento
+     *
+     * @param variable
+     * @param accion
+     * @return
+     * @throws IOException
+     * @throws java.sql.SQLException
+     */
+    public Boolean ComprobarResultadosManteniento(String variable, int accion) throws IOException, SQLException {
+        boolean prueba = false;
+
+        String sqli = """
+                                      SELECT * FROM public.mantenimiento
+                                       ORDER BY idmantenimiento ASC""";
+
+        ps = con.prepareStatement(sqli);
+        rs = ps.executeQuery();
+
+        while (rs.next()) {
+            switch (accion) {
+                case ELIMINAR:
+                    if (!variable.equalsIgnoreCase(rs.getString("nombre"))) {
+                        prueba = true;
+                    }
+                case INSERTAR:
+                    if (variable.equalsIgnoreCase(rs.getString("nombre"))) {
+                        prueba = true;
+                    }
+                case MODIFICAR:
+                    if (variable.equalsIgnoreCase(rs.getString("kilometros_mantenimiento"))) {
+                        prueba = true;
+                    }
+            }
+        }
+
+        rs.close();
+        ps.close(); // Cerrar el PreparedStatement
+        cn.cerrarConexion();
+        return prueba;
+
+    }
+
+    /**
+     * Prueba de resultado Octavo Test para Eliminar un combustible Prueba de
+     * resultado Octavo Test para Listar un combustible
+     *
+     * @param variable
+     * @param accion
+     * @return
+     * @throws IOException
+     * @throws java.sql.SQLException
+     */
+    public Boolean ComprobarResultadosCombustible(String variable, int accion) throws IOException, SQLException {
+        boolean prueba = false;
+
+        String sqli = """
+                                      SELECT * FROM public.combustible
+                                       ORDER BY idcombustible ASC""";
+
+        ps = con.prepareStatement(sqli);
+        rs = ps.executeQuery();
+
+        while (rs.next()) {
+            switch (accion) {
+                case ELIMINAR:
+                    if (!variable.equalsIgnoreCase(rs.getString("nombre"))) {
+                        prueba = true;
+                    }
+                case INSERTAR:
+                    if (variable.equalsIgnoreCase(rs.getString("nombre"))) {
+                        prueba = true;
+                    }
+
+                case MODIFICAR:
+                    if (variable.equalsIgnoreCase(String.valueOf(rs.getFloat("precio")))) {
+                        prueba = true;
+                    }
+
+            }
+        }
+
+        rs.close();
+        ps.close(); // Cerrar el PreparedStatement
+        cn.cerrarConexion();
+        return prueba;
+
+    }
+
+    /**
+     * prueba de resultado Tercer Test para Eliminar un empleado prueba de
+     * resultado Tercer Test para Listar un empleado
+     *
+     * @param variable
+     * @param accion
+     * @return
+     * @throws IOException
+     * @throws java.sql.SQLException
+     */
+    public Boolean ComprobarResultadosConductor(String variable, int accion) throws IOException, SQLException {
+        boolean prueba = false;
+
+        String sqli = """
+                                      SELECT * FROM public.conductor
+                                       ORDER BY idconductor ASC""";
+
+        ps = con.prepareStatement(sqli);
+        rs = ps.executeQuery();
+
+        while (rs.next()) {
+
+            if (accion == MODIFICAR) {
+                if (variable.equalsIgnoreCase(rs.getString("fecha_caducidad_carnet"))) {
+                    prueba = true;
+                }
+            }
+        }
+        rs.close();
+        ps.close(); // Cerrar el PreparedStatement
+        cn.cerrarConexion();
+
+        return prueba;
+
+    }
+
+}
