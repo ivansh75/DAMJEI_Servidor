4656690651c282b55e652d9381d17d6c1cb9db51
ivansh75 <126617261+ivansh75@users.noreply.github.com>
2023-05-23 20:13:19 +0200

    23/05/2023 Por fin!!!
    Actualizado el error de fechas.....y OK!!

---
+++ src/main/java/damjei_BD/SqlCombustible.java
@@ -0,0 +1,220 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package damjei_BD;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import static damjei_GestionClases.GestionCombustible.COMBUSTIBLE;
+import static damjei_GestionClases.GestionCombustible.IDCOMBUSTIBLE;
+import static damjei_GestionClases.GestionCombustible.PRECIO;
+import static damjei_GestionClases.GestionEmpleados.NOMBRE;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Ivimar
+ */
+public class SqlCombustible {
+
+    String resposta;
+    boolean conexio = false;
+
+    /**
+     *
+     * Constructor Simple
+     */
+    public SqlCombustible() {
+    }
+
+    /**
+     * Insertamos Combustible
+     *
+     * @param datos
+     * @return
+     * @throws SQLException
+     * @throws ParseException
+     */
+    public String InsertarCombustible(ArrayList<String> datos) throws SQLException, ParseException {
+        Gson gson = new Gson();
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        //int idcombustible = Integer.parseInt("nextval ('sec_idcombustible')");
+        String nombre = datos.get(1);
+        float precio = Float.parseFloat(datos.get(2));
+
+        ConexionBD cn = new ConexionBD();
+        Connection con = cn.establecerConexion();
+
+        String sql = """
+                         INSERT INTO public.combustible(
+                         idcombustible, nombre, precio)
+                         VALUES (nextval('sec_idcombustible'), ?, ?)""";
+
+        ps = con.prepareStatement(sql);
+        ps.setString(1, nombre);
+        ps.setFloat(2, precio);
+
+        int result = ps.executeUpdate();
+        conexio = result != 0; //si la conexion obtiene un resultado conexio = true sino false
+        JsonObject obtResposta = new JsonObject();
+        obtResposta.addProperty("correcte", conexio);
+        resposta = gson.toJson(obtResposta);
+
+        ps.close(); // Cerrar el PreparedStatement
+        cn.cerrarConexion();
+        return resposta;
+
+    }
+
+    /**
+     * Eliminamos el Combustible de la matricula que nos dan
+     *
+     * @param nombre
+     * @return
+     * @throws SQLException
+     */
+    public String EliminarCombustible(String nombre) throws SQLException {
+        Gson gson = new Gson();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        ConexionBD cn = new ConexionBD();
+        Connection con = cn.establecerConexion();
+
+        String sql = """
+                     DELETE From public.combustible
+                     WHERE UPPER(nombre) = UPPER(?) """;
+
+        ps = con.prepareStatement(sql);
+        //ps.setInt(1,idempleado);
+        ps.setString(1, nombre);
+
+        int result = ps.executeUpdate();
+        conexio = result != 0; //si la conexion obtiene un resultado conexio = true sino false
+        JsonObject obtResposta = new JsonObject();
+        obtResposta.addProperty("correcte", conexio);
+        resposta = gson.toJson(obtResposta);
+
+        ps.close(); // Cerrar el PreparedStatement
+        cn.cerrarConexion();
+        System.out.println(result);
+        return resposta;
+
+    }
+
+    /**
+     * obtenemos la clase a la que se quiere listar o eistar uno porel idcombustible
+     *
+     * @param variable
+     * @return
+     * @throws java.sql.SQLException
+     */
+    public String ListarCombustible(String variable) throws SQLException {
+        Gson gson = new Gson();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        ConexionBD cn = new ConexionBD();
+        Connection con = cn.establecerConexion();
+
+        if (variable.equalsIgnoreCase(COMBUSTIBLE)) {
+
+            String sqlc = "SELECT * FROM " + variable + "\n ORDER BY idcombustible ASC ";
+            ps = con.prepareStatement(sqlc);
+
+        } else {
+
+            String sqli = """
+                 SELECT * FROM public.combustible
+                 WHERE idcombustible = ?""";
+
+            ps = con.prepareStatement(sqli);
+            ps.setInt(1, Integer.parseInt(variable));
+        }
+
+        rs = ps.executeQuery();
+        
+        // Creamos un objeto JsonArray para almacenar los resultados
+        JsonArray jsonArray = new JsonArray();
+
+        while (rs.next()) {
+            JsonObject jsonObject = new JsonObject();
+
+            jsonObject.addProperty(IDCOMBUSTIBLE, rs.getInt(IDCOMBUSTIBLE));
+            jsonObject.addProperty(NOMBRE, rs.getString(NOMBRE));
+            jsonObject.addProperty(PRECIO, rs.getFloat(PRECIO));
+
+            jsonArray.add(jsonObject);
+        }
+
+        if (jsonArray.size() == 0) {
+            conexio = false;
+            JsonObject obtResposta = new JsonObject();
+            obtResposta.addProperty("correcte", conexio);
+            resposta = gson.toJson(obtResposta);
+        } else {
+            resposta = gson.toJson(jsonArray);
+        }
+
+        cn.cerrarConexion();
+        return resposta;
+    }
+
+    /**
+     *
+     * Modificamos en el atributo que nos pasan con el valor variable
+     *
+     * @param nombre
+     * @param variable
+     * @param atributo
+     * @return
+     * @throws SQLException
+     * @throws java.text.ParseException
+     */
+    public String ModificarCombustible(String nombre, String variable, String atributo) throws SQLException, ParseException {
+        Gson gson = new Gson();
+
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        ConexionBD cn = new ConexionBD();
+        Connection con = cn.establecerConexion();
+
+        String sql = "UPDATE public.combustible "
+                + " SET " + atributo + " = ? "
+                + "WHERE UPPER(nombre) = UPPER(?)";
+
+        ps = con.prepareStatement(sql);
+
+        ps.setFloat(1, Float.parseFloat(variable));
+        ps.setString(2, nombre);
+
+        int result = ps.executeUpdate();
+
+        if (result != 0) {
+            conexio = true;
+            JsonObject obtResposta = new JsonObject();
+            obtResposta.addProperty("correcte", conexio);
+            resposta = gson.toJson(obtResposta);
+
+            ps.close(); // Cerrar el PreparedStatement
+            cn.cerrarConexion();
+            return resposta;
+        }
+
+        ps.close(); // Cerrar el PreparedStatement
+        cn.cerrarConexion();
+        return resposta = gson.toJson(conexio);
+    }
+
+}
---
+++ src/main/java/damjei_GestionClases/GestionRepostar.java
@@ -0,0 +1,162 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package damjei_GestionClases;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import damjei_BD.SqlRepostar;
+import damjei_ConexionCliente.ClientRequest;
+import static damjei_GestionClases.GestionEmpleados.ELIMINAR;
+import static damjei_GestionClases.GestionEmpleados.INSERTAR;
+import static damjei_GestionClases.GestionEmpleados.LISTAR;
+import static damjei_GestionClases.GestionEmpleados.LISTARID;
+import static damjei_GestionClases.GestionEmpleados.MODIFICAR;
+import java.sql.SQLException;
+import java.text.ParseException;
+
+/**
+ *
+ * @author Ivimar
+ */
+public class GestionRepostar {
+
+    public static final String REPOSTAR = "repostar";
+    //constantes de los titulos atributos
+    public static final String IDREPOSTAR = "idrepostar";
+    public static final String FECHA_REPOSTAR = "fecha_repostar";
+    public static final String IMPORTE_REPOSTAR = "importe_repostar";
+    public static final String KILOMETROS_REPOSTAR = "kilometros_repostar";
+    public static final String COMBUSTIBLEID = "combustibleid";
+    public static final String VEHICULOID = "vehiculoid";
+    public static final String CONDUCTORID = "conductorid";
+    public static final String LITROS = "litros";
+    public static final String DATOS_REPOSTAR = "repostar";
+    public static final String DATOS_CLASE = "clase";
+    public static final String DATOS_ACCION = "accio";
+    public static final String DATOS_TOKEN = "token";
+
+    public static final String ERROR_GESTIONREPOSTAR = "Error en la gestion de Repostar";
+
+    String respostaRep;
+    boolean conexio = false;
+
+    public GestionRepostar() {
+    }
+
+    public String GestionRepostar(String json) throws SQLException, ParseException {
+
+        Gson gson = new Gson();
+        ClientRequest request = new ClientRequest();
+        JsonObject jsonObject = new Gson().fromJson(json, JsonObject.class);
+
+        // Obtener el objeto Vehicle del JSON 
+        JsonObject repostarJson = jsonObject.getAsJsonObject(REPOSTAR);
+        if (repostarJson != null && !repostarJson.isJsonNull()) {
+            request.setAccion(jsonObject.get(DATOS_ACCION).getAsInt());
+
+            switch (request.getAccion()) {
+                case INSERTAR: {
+                    //ArrayList<String> datos = new ArrayList<>();
+                    request.setDatos(repostarJson.get(String.valueOf(IDREPOSTAR)).getAsString());
+                    request.setDatos(repostarJson.get(FECHA_REPOSTAR).getAsString());
+                    request.setDatos(repostarJson.get(String.valueOf(IMPORTE_REPOSTAR)).getAsString());
+                    request.setDatos(repostarJson.get(String.valueOf(KILOMETROS_REPOSTAR)).getAsString());
+                    request.setDatos(repostarJson.get(String.valueOf(COMBUSTIBLEID)).getAsString());
+                    request.setDatos(repostarJson.get(String.valueOf(VEHICULOID)).getAsString());
+                    request.setDatos(repostarJson.get(String.valueOf(CONDUCTORID)).getAsString());
+                    request.setDatos(repostarJson.get(String.valueOf(LITROS)).getAsString());
+
+                    SqlRepostar sql = new SqlRepostar();
+                    //para asignar valores el 0 el el array, el 1 idempleado......
+                    String rs = sql.InsertarRepostar(request.getDatos());
+                    respostaRep = rs;
+                    return respostaRep;
+
+                }
+                case ELIMINAR: {
+                    request.setString1(repostarJson.get(String.valueOf(IDREPOSTAR)).getAsString());
+
+                    SqlRepostar sql = new SqlRepostar();
+                    String rs = sql.EliminarRepostar(request.getString1());
+                    if (!rs.isEmpty()) {
+                        respostaRep = rs;
+                        return respostaRep;
+                    } else {
+                        String error = ("correcta: " + conexio + " error: " + ERROR_GESTIONREPOSTAR);
+                        return respostaRep = gson.toJson(error);
+                    }
+                }
+                case LISTAR: {
+
+                    SqlRepostar sql = new SqlRepostar();
+                    String rs = sql.ListarRepostar(REPOSTAR);
+                    if (!rs.isEmpty()) {
+                        respostaRep = rs;
+                        return respostaRep;
+                    } else {
+                        String error = ("correcta:" + conexio + " error: " + ERROR_GESTIONREPOSTAR);
+                        return respostaRep = gson.toJson(error);
+                    }
+                }
+
+                case MODIFICAR: {
+
+                    request.setString1(repostarJson.get(String.valueOf(IDREPOSTAR)).getAsString());
+
+                    if (repostarJson.get(FECHA_REPOSTAR) != null) {
+                        request.setString2(repostarJson.get(FECHA_REPOSTAR).getAsString());
+                        request.setString3(FECHA_REPOSTAR);
+                    } else if (repostarJson.get(String.valueOf(IMPORTE_REPOSTAR)) != null) {
+                        request.setString2(repostarJson.get(IMPORTE_REPOSTAR).getAsString());
+                        request.setString3(IMPORTE_REPOSTAR);
+                    } else if (repostarJson.get(String.valueOf(KILOMETROS_REPOSTAR)) != null) {
+                        request.setString2(repostarJson.get(String.valueOf(KILOMETROS_REPOSTAR)).getAsString());
+                        request.setString3(KILOMETROS_REPOSTAR);
+                    } else if (repostarJson.get(String.valueOf(COMBUSTIBLEID)) != null && repostarJson.get(COMBUSTIBLEID).getAsInt() != 0) {
+                        request.setString2(repostarJson.get(String.valueOf(COMBUSTIBLEID)).getAsString());
+                        request.setString3(COMBUSTIBLEID);
+                    } else if (repostarJson.get(String.valueOf(VEHICULOID)) != null && repostarJson.get(VEHICULOID).getAsInt() != 0) {
+                        request.setString2(repostarJson.get(String.valueOf(VEHICULOID)).getAsString());
+                        request.setString3(VEHICULOID);
+                    } else if (repostarJson.get(String.valueOf(CONDUCTORID)) != null && repostarJson.get(CONDUCTORID).getAsInt() != 0) {
+                        request.setString2(repostarJson.get(String.valueOf(CONDUCTORID)).getAsString());
+                        request.setString3(CONDUCTORID);
+                    } else if (repostarJson.get(String.valueOf(LITROS)) != null) {
+                        request.setString2(repostarJson.get(String.valueOf(LITROS)).getAsString());
+                        request.setString3(LITROS);
+                    }
+
+                    SqlRepostar sql = new SqlRepostar();
+                    String rs = sql.ModificarRepostar(request.getString1(), request.getString2(), request.getString3());
+                    if (!rs.isEmpty()) {
+                        respostaRep = rs;
+                        return respostaRep;
+                    } else {
+                        String error = ("correcta:" + conexio + " error: " + ERROR_GESTIONREPOSTAR);
+                        return respostaRep = gson.toJson(error);
+                    }
+                }
+                case LISTARID: {
+                    request.setString1(repostarJson.get(String.valueOf(IDREPOSTAR)).getAsString());
+
+                    SqlRepostar sql = new SqlRepostar();
+                    String rs = sql.ListarRepostar(request.getString1());
+                    if (!rs.isEmpty()) {
+                        respostaRep = rs;
+                        return respostaRep;
+                    } else {
+                        String error = ("correcta:" + conexio + " error: " + ERROR_GESTIONREPOSTAR);
+                        return respostaRep = gson.toJson(error);
+                    }
+                }
+            }
+
+        }
+
+        String error = ("correcta: " + conexio + " error: " + ERROR_GESTIONREPOSTAR);
+        return respostaRep = gson.toJson(error);
+    }
+
+}
